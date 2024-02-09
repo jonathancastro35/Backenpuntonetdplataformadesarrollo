7f7e4840179fc7b1254c3c65ee7adb51de3ded5a
Jonathan Castro Cardona <62910104+jonathancastro35@users.noreply.github.com>
2024-02-09 17:53:29 -0500

    subiendo el backend

---
+++ Trabajaounotareasusuarios/Trabajaounotareasusuarios/Controllers/LoginjefesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Trabajaounotareasusuarios.Context;
+using Trabajaounotareasusuarios.Models;
+
+namespace Trabajaounotareasusuarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginjefesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LoginjefesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Tarea>>> PostLogin(Loginjefe loginjefe)
+        {
+            if (JefeusertExists(loginjefe.Usuario, loginjefe.Pass).Equals(true))
+            {
+
+                var x = await _context.Tareas.ToListAsync();
+
+                List<Tarea> listadotareas = new List<Tarea>();
+                long conttareas = x.LongCount();
+
+                if(conttareas > 0)
+                {
+                    for (var i = 0; i < x.LongCount(); i++)
+                    {
+
+                        listadotareas.Add(new Tarea() { Id = x[i].Id, Idproyectoempleado = x[i].Idproyectoempleado, Fechatarea = x[i].Fechatarea, Horaentrada = x[i].Horaentrada, Horasalida = x[i].Horasalida, Nombretarea = x[i].Nombretarea, Tiempodedicadotarea = x[i].Tiempodedicadotarea });
+
+
+                    }
+
+                    return listadotareas;
+
+                }
+
+                listadotareas.Add(new Tarea() { Id = 0, Idproyectoempleado = 0, Fechatarea = DateTime.Now, Horaentrada = 0, Horasalida = 0, Nombretarea = "No hay tareas", Tiempodedicadotarea = 0 });
+                return listadotareas;
+
+
+
+
+            }
+
+            return NotFound();
+        }
+
+
+
+        private bool JefeusertExists(string usuario, string pass)
+        {
+
+            return _context.Jefes.Any(e => e.Usuario == usuario && e.Pass == pass);
+        }
+
+       
+    }
+}
+
+
---
+++ Trabajaounotareasusuarios/Trabajaounotareasusuarios/Models/Registrotareasenproyectofecha.cs
@@ -0,0 +1,15 @@
+﻿namespace Trabajaounotareasusuarios.Models
+{
+    public class Registrotareasenproyectofecha
+    {
+        public int Id { get; set; }
+        public required string Nombreproyecto { get; set; }
+        public int Horastrabajadasenproyecto { get; set; }
+
+        public required string Horastotales { get; set; }
+
+        public int Idempleado { get; set; }
+        public DateTime Fechatarea { get; set; }
+    }
+}
+Id, Nombreproyecto, Horastrabajadasenproyecto, Horastotales, Idempleado, Fechatarea
